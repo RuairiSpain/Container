f3f307a5de5b080d5d5201764a4459be22a08564
RuairiSpain <lapido@gmail.com>
2016-11-13 13:50:50 +0100

    test web app

--- index.js
+++ index.js
@@ -1,64 +1,15 @@
-var express = require('express'),
-    app = express(),
-    pkg = require('./package.json'),
-    bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    users = require('./lib/users')(getDB);
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
 
-module.exports = app;
-
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-
-function getDB() {
-  return app.get('db');
-}
+//DB setup
+mongoose.connect('mongodb://mongo:27017');
 
 app.get('/', function(req, res) {
-	res.json({ name: pkg.name, version: pkg.version });
+    res.send('Hello World-changed-1 ');
 });
 
-app.get('/users', function(req, res) {
 
-  users.getUsers(function(err, result) {
-    if (err) {
-      // just an example (we don't actually throw any errors in getUsers)
-      return res.status(500).json( { success: false, reason: err.message });
-    }
-
-    res.send({ success: true, users: result });
-  });
-
-});
-
-app.get('/users/:id', function(req, res) {
-  var id = req.params.id;
-
-  users.getUser(id, function(err, result) {
-    if (err) {
-      // just an example (bad request)
-      return res.status(400).json( { success: false, reason: err.message });
-    }
-
-    if (!result) {
-      return res.status(404).json( { success: false, reason: 'user id not found' });
-    }
-
-    res.send({ success: true, user: result });
-  });
-
-});
-
-app.post('/users', function(req, res) {
-  var user = req.body;
-
-  users.addUser(user, function(err, result) {
-    if (err) {
-      // just an example (bad request) since the only error that we throw is if missing user name
-      return res.status(400).json( { success: false, reason: err.message });
-    }
-
-    res.send({ success: true, user: result });
-  });
-
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Example app listening on port 3000!');
 });
